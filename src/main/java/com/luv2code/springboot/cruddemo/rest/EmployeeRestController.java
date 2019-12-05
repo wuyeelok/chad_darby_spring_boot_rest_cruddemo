4d5477fe198738e355060c4711abfffebe99d0cc
wuyeelok <wuyeelok@gmail.com>
2019-12-05 15:33:47 +0800

    Refactor: Add service layer.  Only findall functio

--- src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -7,24 +7,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
 import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-	private final EmployeeDAO employeeDAO;
+	private final EmployeeService employeeService;
 
 	// Quick & Dirty: Inject Employee DAO
 	@Autowired
-	public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
-		this.employeeDAO = theEmployeeDAO;
+	public EmployeeRestController(EmployeeService employeeService) {
+		this.employeeService = employeeService;
 	}
 
 	// Expose "/employees" & return list of employees
 	@GetMapping("/employees")
 	public List<Employee> findAll() {
-		return this.employeeDAO.findAll();
+		return this.employeeService.findAll();
 	}
 }
