dc76f3d7d0868de7e723edc75bf06c5edd8405d9
wuyeelok <wuyeelok@gmail.com>
2019-12-06 15:25:28 +0800

    REST: Able to create new employee

--- src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,6 +40,20 @@ public class EmployeeRestController {
 		if (theEmployee == null) {
 			throw new RuntimeException("Employee id not found - " + eId);
 		}
+
+		return theEmployee;
+	}
+
+	// Add mapping for POST /employees - add new employee
+	@PostMapping("/employees")
+	public Employee addEmployee(@RequestBody Employee theEmployee) {
+
+		// Also just in case user pass an id in JSON, set id to 0
+		// This is to force a save of new item instead of update
+
+		theEmployee.setId(0);
+
+		this.employeeService.save(theEmployee);
 
 		return theEmployee;
 	}
