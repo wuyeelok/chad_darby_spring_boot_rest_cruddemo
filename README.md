373dd76e82fe9dc728b48a0b5905c36da57fa6c5
wuyeelok <wuyeelok@gmail.com>
2019-12-13 16:23:19 +0800

    Setup: Init DB, table & testing data when start

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 ## Reference URL
 
 [Entry Link: Spring Boot cruddemo](https://www.udemy.com/course/spring-hibernate-tutorial/learn/lecture/12940900#overview)
+
+
+# How to start the project
+
+1. Install **maven**
+2. Start **MySQL**
+3. Run sql inside ```mysql_script``` folder
+4. In *project root path*, run ```mvn spring-boot:run``` (use ```Ctrl C``` to stop) or *use IDE to run*
+5. In *browser* or *postman*, ```http://localhost:10789/magic-api/members```
--- src/main/java/com/luv2code/springboot/cruddemo/CruddemoApplication.java
+++ src/main/java/com/luv2code/springboot/cruddemo/CruddemoApplication.java
@@ -1,13 +1,44 @@
 package com.luv2code.springboot.cruddemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.luv2code.springboot.cruddemo.dao.EmployeeRepository;
+import com.luv2code.springboot.cruddemo.entity.Employee;
 
 @SpringBootApplication
 public class CruddemoApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(CruddemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
 
+	// Testing only, create data in DB when server start
+	@Bean
+	public CommandLineRunner dbDataInit(EmployeeRepository employeeRepository) {
+		return (args) -> {
+
+			// create dummy employee
+			log.info("Start to create dummy employee...");
+			employeeRepository.save(new Employee("Leslie", "Andrews", "leslie@luv2code.com"));
+			employeeRepository.save(new Employee("Emma", "Baumgarten", "emma@luv2code.com"));
+			employeeRepository.save(new Employee("Avani", "Gupta", "avani@luv2code.com"));
+			employeeRepository.save(new Employee("Yuri", "Petrov", "yuri@luv2code.com"));
+			employeeRepository.save(new Employee("Juan", "Vega", "juan@luv2code.com"));
+
+			// fetch all employee
+			log.info("Employee found with findAll():");
+			log.info("---------------------------");
+			for (Employee employee : employeeRepository.findAll()) {
+				log.info(employee.toString());
+			}
+
+		};
+	}
 }
